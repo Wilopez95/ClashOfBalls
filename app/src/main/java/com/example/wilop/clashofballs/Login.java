baea22cf7471b1ae5ec248d4439fe55c5a5bb4ce
Wilopez <Wilopez95@gmail.com>
2018-05-25 14:54:41 -0600

    Second commit

--- app/src/main/java/com/example/wilop/clashofballs/Login.java
+++ app/src/main/java/com/example/wilop/clashofballs/Login.java
@@ -1,23 +1,43 @@
 package com.example.wilop.clashofballs;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     TextView text_error,txt_registro;
     EditText Correo,Contraseña;
     ImageButton Login;
 
+    FirebaseAuth mAuth;
+
+
+    @Override
+        protected void onStart() {
+        super.onStart();
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
         text_error = findViewById(R.id.text_error);
         Correo = findViewById(R.id.correo);
         Contraseña = findViewById(R.id.contraseña);
@@ -38,11 +58,52 @@ public class Login extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
+                String txtcorreo = Correo.getText().toString();
+                String txtcontra = Contraseña.getText().toString();
+                if (txtcorreo.isEmpty()||txtcontra.isEmpty()){
+                    text_error.setText("Datos invalidos");
+                    text_error.setVisibility(View.VISIBLE);
+                }else {
+                    text_error.setVisibility(View.INVISIBLE);
+                    loginUser();
+
+
+                }
+
 
             }
         });
 
+    }
+    private void loginUser(){
+        String txtcorreo = Correo.getText().toString();
+        String txtcontra = Contraseña.getText().toString();
+        mAuth.signInWithEmailAndPassword(txtcorreo, txtcontra)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+
+        } else {
+            text_error.setText("Datos invalidos");
+            text_error.setVisibility(View.VISIBLE);
+            //Toast.makeText(Login.this, "Error al iniciar sesion",
+                    //Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/wilop/clashofballs/Registro.java
+++ app/src/main/java/com/example/wilop/clashofballs/Registro.java
@@ -1,16 +1,27 @@
 package com.example.wilop.clashofballs;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registro extends AppCompatActivity {
     EditText Nombre,Correo,Contraseña,Contraseña_conf;
     TextView Msj_error;
     ImageButton Registro;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,7 @@ public class Registro extends AppCompatActivity {
         Contraseña_conf = findViewById(R.id.r_contraseña_confirmar);
         Msj_error = findViewById(R.id.texto_registro);
         Registro = findViewById(R.id.registro);
+        mAuth = FirebaseAuth.getInstance();
 
         Registro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +49,7 @@ public class Registro extends AppCompatActivity {
                     if (txtcontra.equals(txtcontra_conf)){
                         if(txtcontra.length()>6){
                             Msj_error.setVisibility(View.INVISIBLE);
-                            //DO TODO
+                            registro();
                         }else{
                             Msj_error.setVisibility(View.VISIBLE);
                             Msj_error.setText("Las contraseñas es demasiado corta");
@@ -51,5 +63,31 @@ public class Registro extends AppCompatActivity {
             }
         });
 
+    }
+    private void registro(){
+        String txtnombre = Nombre.getText().toString();
+        String txtcorreo= Correo.getText().toString();
+        String txtcontra = Contraseña.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(txtcorreo,txtcontra)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(Registro.this, "Cuenta creada",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), Login.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Registro.this, "Error al crear la cuenta",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
